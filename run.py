ec979d3d52bfb31fa30a22db83d394d2fb3369ce
bartekburnat7 <>
2023-08-10 18:02:02 +0000

    add a main function and a tax_calculate which returns the amount of tax you need to pay.

--- run.py
+++ run.py
@@ -14,6 +14,29 @@ SHEET = GSPREAD_CLIENT.open('python_project')
 
 sales = SHEET.worksheet('project3')
 
-data = sales.get_all_values()
+def tax_calculate(x):
+    print("Calculating....")
+    if x <= 12570:
+        tax_pay = 0
+        return tax_pay
 
-print(data)
+    elif x >= 12571 and x <= 50270:
+        tax_pay = 0.2 * (x - 12570)
+        return tax_pay
+
+    elif x >= 50271 and x <= 125140:
+        tax_pay = 7539.8 + 0.4 * (x - 50271)
+        return tax_pay
+
+    elif x > 125140:
+        tax_pay = 29947.6 + 0.45 * (x - 125140)
+        return tax_pay
+
+def main():
+    user_name = input("Enter your Name: ")
+    user_earn = input("How much you earn a year: £")
+    user_earn = int(user_earn)
+    print(tax_calculate(user_earn))
+    
+
+main()
